6e5850ef9507a44687d4bcf2d3cfa63388309c80
Benjamin <legoawesomepants@gmail.com>
2022-03-03 11:43:58 -0600

    Merge

--- generateRandomUsername.js
+++ generateRandomUsername.js
@@ -1,6 +1,6 @@
-const firstNames = ['Chet','Ezra','Robert','Amuro','Heero','Eren','Wallace','Shrek','Barrack','Donald','George','Char','Casval','Gromit','Sayla','Bright','Remy','Linguini','Matilda','Frau','Buzz','Luke','Darth'];
-const middleNames = ['Bob','Chatter','Room','Mature','Immature','Noisy','Smart','Intelligent','The','Of','AffiliatedWith','UnaffiliatedWith','Representing','With','Without','','Plus'];
-const lastNames = ['Aderhold','Obama','Trump','Bush','Ray','Aznable','Deikun','Noa','Jaeger','Bo','ChatRoom','ChetRoom','Lightyear','Aldrin','Skywalker','Vader'];
+const firstNames = ['Chet','Ezra','Robert','','Amuro','Heero','Eren','Wallace','Shrek','Barrack','Donald','George','Char','Casval','Gromit','Sayla','Bright','Remy','Linguini','Matilda','Frau','Buzz','Luke','Darth'];
+const middleNames = ['Bob','Chatter','Room','Immature','Noisy','Smart','Intelligent','The','Of','AffiliatedWith','UnaffiliatedWith','Representing','With','Without','','Plus'];
+const lastNames = ['Aderhold','Obama','Trump','','Bush','Ray','Aznable','Deikun','Noa','Jaeger','Bo','ChatRoom','ChetRoom','Lightyear','Aldrin','Skywalker','Vader'];
 function generateRandomUsername(){
     const randomFirstName = firstNames[Math.floor((Math.random() * firstNames.length))];
     const randomSecondName = middleNames[Math.floor((Math.random() * middleNames.length))];
